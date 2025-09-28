162ac5d13743e1c3270f4fb3c4e48a72dfb7bd2a
kessiena <kessienaunuakpor@gmail.com>
2025-09-28 13:52:52 +0100

    Final commit (hopefully)

--- hello.py
+++ hello.py
@@ -1,10 +1,36 @@
 from flask import Flask
 
-
 app = Flask(__name__)
 
-
+# Homepage
 @app.route('/')
+def home():
+    return '''
+        <h1>Welcome to my Flask App!</h1>
+        <p>Hello, World, I am a Flask app!</p>
+        <p>
+            <a href="/about">About</a> | 
+            <a href="/contact">Contact</a>
+        </p>
+    '''
 
-def say_hello():
-	return '<p>Hello, World, I am a Flask app!</p>'
+# About page
+@app.route('/about')
+def about():
+    return '''
+        <h1>About</h1>
+        <p>This is the about page of my Flask project.</p>
+        <p><a href="/">Back to Home</a></p>
+    '''
+
+# Contact page
+@app.route('/contact')
+def contact():
+    return '''
+        <h1>Contact</h1>
+        <p>You can reach me at: <strong>kessienaunuakpor@gmail.com</strong></p>
+        <p><a href="/">Back to Home</a></p>
+    '''
+
+if __name__ == '__main__':
+    app.run(debug=True)
